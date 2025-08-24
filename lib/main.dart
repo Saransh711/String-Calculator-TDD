1ee8ead6e70523e8c799b777063c6230dd4b1bb6
Saransh <saransh@Saranshs-MacBook-Air.local>
2025-08-24 18:54:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'String Calculator TDD',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello Flutter')),
+      body: const Center(
+        child: Text('Welcome to Flutter!', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
